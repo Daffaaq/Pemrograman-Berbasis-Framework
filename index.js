2f9bd069e3637dfe3ba2d390501be76e1633e90a
Daffaaq <daffaaqila48@gmail.com>
2023-02-13 20:29:07 +0700

    step 08 side effect

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let state = {
-    inputValue: "",
+    inputValue: localStorage.getItem("inputValue") ?? "",
     hash: location.hash,
 };
 
 function setState(newState) {
-    state = {...state, ...newState};
+    const prevState = {...state};
+    const nextState = {...state, ...newState};
+    state = nextState;
     render();
+    onStateChange(prevState, nextState);
+}
+
+function onStateChange(prevState, nextState) {
+    if(prevState.inputValue !== nextState.inputValue) {
+        localStorage.setItem("inputValue", nextState.inputValue);
+    }
+
+    if(prevState.hash !== nextState.hash) {
+        history.pushState(null, "",nextState.hash);
+    }
 }
 
 function Link(props){
@@ -15,8 +28,7 @@ function Link(props){
     link.onclick = function(event) {
         event.preventDefault();
         const url = new URL(event.target.href);
-        setState({hash: url.hash});
-        history.pushState(null, "",event.target.href)
+        setState({hash: url.hash});  
         render();
     };  
 
